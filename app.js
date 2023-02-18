02c056565c1f4197682ce48976ffaf17668f811b
Carlos Villegas <carlos.villegasch@gmail.com>
2023-02-17 21:41:12 -0500

    Installation express-ejs-layouts, creation of views and public folders

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 
+//Invocamos a express-ejs-layouts
+const expressLayouts = require('express-ejs-layouts')
+
 //Configuramos el motor de plantillas EJS
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
+//Usamos expressLayouts
+app.use(expressLayouts)
+
 app.get('/', (req, res)=>{
-    res.send('LAYOUTS CON NODE')
+    res.render('inicio')
+})
+
+app.get('/contacto', (req, res)=>{
+    res.render('contacto')
 })
 
 app.listen(3000, ()=> {
